1591fb7b569aa8e468f7d550c900a0d29426b195
rkchy23 <101350205+rkchy23@users.noreply.github.com>
2023-06-26 00:22:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './style/App.css';
+import ToolTip from './ToolTip';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <ToolTip position='top' />
+        <ToolTip position='bottom' />
+      </div>
+      <div>
+        <ToolTip position='left' />
+        <ToolTip position='right' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
